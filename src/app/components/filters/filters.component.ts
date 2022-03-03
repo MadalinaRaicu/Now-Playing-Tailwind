0fb09c12e942eac47ca386bac12d8bd3aec8aa31
madalinaraicu <madaraicu@gmail.com>
2022-03-03 22:13:03 +0000

    add clear all filters

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -11,32 +11,39 @@ export class FiltersComponent implements OnInit {
   @Input() genres!: Genre[];
   @Output() filterChanged = new EventEmitter<number[]>();
 
-  form: FormGroup;
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   get genresFormArray() {
     return this.form.controls['genres'] as FormArray;
   }
 
-  constructor(private formBuilder: FormBuilder) {
+  ngOnInit(): void {
     this.form = this.formBuilder.group({
       genres: new FormArray([]),
     });
+    this.addCheckboxes();
   }
 
-  ngOnInit(): void {
+  clearFilters() {
+    this.form = this.formBuilder.group({
+      genres: new FormArray([]),
+    });
     this.addCheckboxes();
+    this.filterChanged.emit([]);
+  }
+
+  updateFilters() {
+    const selectedGenreIds = this.form.value.genres
+      .map((checked: any, i: number) => (checked ? this.genres[i].id : null))
+      .filter((v: null) => v !== null);
+    this.filterChanged.emit(selectedGenreIds);
   }
 
   private addCheckboxes() {
     this.genres.forEach(() =>
       this.genresFormArray.push(new FormControl(false))
     );
-  }
-
-  submit() {
-    const selectedGenreIds = this.form.value.genres
-      .map((checked: any, i: number) => (checked ? this.genres[i].id : null))
-      .filter((v: null) => v !== null);
-    this.filterChanged.emit(selectedGenreIds);
   }
 }
