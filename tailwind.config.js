1f39357b03ee4f084e9f50d5970431443153ced4
madalinaraicu <madaraicu@gmail.com>
2022-03-04 00:08:59 +0000

    add tailwind and fix styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts,scss,css,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: "#f64f9e",
+        "gray-50": "#f3f3f3",
+      },
+    },
+  },
+  plugins: [],
+};
